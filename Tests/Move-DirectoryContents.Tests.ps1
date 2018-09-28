108c23ed1a3b02121fe3919a64b552d3c71348bf
Lixquid <Lixquid@users.noreply.github.com>
2018-09-28 15:47:44 +0100

    Formatted tests to use pester after blocks

--- Tests/Move-DirectoryContents.Tests.ps1
+++ Tests/Move-DirectoryContents.Tests.ps1
@@ -3,7 +3,7 @@ Import-Module Remove-DirectoryContents -Force
 Import-Module Move-DirectoryContents -Force
 
 Describe "Move-DirectoryContents" {
-    function setup {
+    BeforeEach {
         New-Item TestDrive:\from -ItemType Directory
 
         New-Item TestDrive:\from\a.txt -ItemType File -Value a
@@ -11,12 +11,11 @@ Describe "Move-DirectoryContents" {
         New-Item TestDrive:\from\subdir -ItemType Directory
         New-item TestDrive:\from\subdir\sub.txt -ItemType File -Value c
     }
-    function teardown {
+    AfterEach {
         Remove-DirectoryContents TestDrive:\from TestDrive:\to -AndRemoveParent
     }
 
     It "Given two paths, the directory's contents will be moved and nothing returned" {
-        setup
         New-Item TestDrive:\to -ItemType Directory
         Move-DirectoryContents TestDrive:\from TestDrive:\to | Should -BeNullOrEmpty
         Get-ChildItem TestDrive:\from | Should -HaveCount 0
@@ -24,24 +23,19 @@ Describe "Move-DirectoryContents" {
         Get-Content TestDrive:\to\a.txt | Should -Be a
         Get-Content TestDrive:\to\b.txt | Should -Be b
         Get-Content TestDrive:\to\subdir\sub.txt | Should -Be c
-        teardown
     }
     It "Given one path, the directory's contents will be moved to a new folder" {
-        setup
         Move-DirectoryContents TestDrive:\from TestDrive:\to
         Get-ChildItem TestDrive:\from | Should -HaveCount 0
         Get-ChildItem TestDrive:\to | Should -HaveCount 3
         Get-Content TestDrive:\to\a.txt | Should -Be a
         Get-Content TestDrive:\to\b.txt | Should -Be b
         Get-Content TestDrive:\to\subdir\sub.txt | Should -Be c
-        teardown
     }
     It "Given one path and -PassThru, the directory's contents will be moved to a new folder and the new path returned" {
-        setup
         $output = Move-DirectoryContents TestDrive:\from TestDrive:\to -PassThru
         $output | Should -Be (Join-Path $TestDrive to)
         Get-ChildItem TestDrive:\from | Should -HaveCount 0
         Get-ChildItem TestDrive:\to | Should -HaveCount 3
-        teardown
     }
 }
--- Tests/Remove-DirectoryContents.Tests.ps1
+++ Tests/Remove-DirectoryContents.Tests.ps1
@@ -2,22 +2,22 @@
 Import-Module Remove-DirectoryContents -Force
 
 Describe "Remove-DirectoryContents" {
-    function setup {
+    BeforeEach {
         New-Item TestDrive:\container -ItemType Directory
         New-Item TestDrive:\container\subfolder -ItemType Directory
 
         New-Item TestDrive:\container\topfile.txt -ItemType File
         New-Item TestDrive:\container\subfolder\subfile.txt -ItemType File
     }
+    AfterEach {
+        Remove-Item TestDrive:\container -Recurse -ErrorAction SilentlyContinue
+    }
 
     It "Given a file path, removes the contents" {
-        setup
         Remove-DirectoryContents TestDrive:\container
         Get-ChildItem TestDrive:\container | Should -HaveCount 0
-        Remove-Item TestDrive:\container -Recurse
     }
     It "Given a file path and -AndRemoveParent, removes the entire folder" {
-        setup
         Remove-DirectoryContents TestDrive:\container -AndRemoveParent
         Test-Path TestDrive:\container | Should -BeFalse
     }
